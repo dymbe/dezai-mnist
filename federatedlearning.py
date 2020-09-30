83f55d8ed5aeaa39fa1de4c3fa109b551ab55293
Erik Dymbe <erik.dymbe@gmail.com>
2020-09-30 09:40:22 +0200

    Work on saving optimizer state
    Not done

--- federatedlearning.py
+++ federatedlearning.py
@@ -8,7 +8,30 @@ import torch.utils.data
 import copy
 from torchvision import datasets, transforms
 from mnistnn import Net
-from utils import most_frequent_in_rows
+from utils import average_models, average_optimizers, most_frequent_in_rows
+
+
+def train_models(models, train_loaders, device, subepochs, optimizer_params=None):
+    models = [model.to(device) for model in models]
+    optimizers = [optim.Adam(model.parameters()) for model in models]
+    if optimizer_params is not None:
+        for optimizer in optimizers:
+            optimizer.param_groups[0]["params"] = optimizer_params
+
+    for i, model in enumerate(models):
+        for epoch in range(subepochs):
+            for (inputs, targets) in train_loaders[i]:
+                inputs, targets = inputs.to(device), targets.to(device)
+                optimizers[i].zero_grad()
+                output = model(inputs)
+                one_hot_targets = F.one_hot(targets, num_classes=10).float()
+                loss = F.mse_loss(output, one_hot_targets)
+                loss.backward()
+                optimizers[i].step()
+
+    avg_params = average_optimizers(optimizers)
+
+    return [model.cpu() for model in models], avg_params
 
 
 def train_and_return(model, train_loader, device, subepochs, plot=True):
@@ -17,7 +40,7 @@ def train_and_return(model, train_loader, device, subepochs, plot=True):
     optimizer = optim.Adam(model.parameters())
     losses = []
     for epoch in range(subepochs):
-        for batch_idx, (inputs, targets) in enumerate(train_loader):
+        for (inputs, targets) in train_loader:
             inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()
             output = model(inputs)
@@ -50,9 +73,7 @@ def test(model, device, test_loader):
 def all_outputs(models, device, test_loader):
     models = [model.to(device) for model in models]
     results = np.empty((len(test_loader.dataset), len(models)))
-    results[:] = np.nan
     all_targets = np.empty((len(test_loader.dataset), 1))
-    all_targets[:] = np.nan
 
     with torch.no_grad():
         y = 0
@@ -68,22 +89,12 @@ def all_outputs(models, device, test_loader):
     return results, all_targets
 
 
-def average_models(models):
-    model = Net()
-    sd = model.state_dict()
-    for key in sd:
-        sd[key] = sum([model.state_dict()[key] for model in models]) / len(models)
-    model.load_state_dict(sd)
-    return model
-
-
 def experiment(batch_size,
                batches_per_client,
                dataset_size,
                testset_size,
                epochs,
-               subepochs,
-               plot):
+               subepochs):
     torch.manual_seed(0)
     device = torch.device("cuda")
     dps_per_client = batch_size * batches_per_client
@@ -105,35 +116,31 @@ def experiment(batch_size,
     if dataset_size < len(x_train):
         train_subset_sizes += [len(x_train) - dataset_size]
 
-    train_subsets = torch.utils.data.random_split(x_train, train_subset_sizes, None)[:n_clients]
-
+    train_subsets = torch.utils.data.random_split(x_train, train_subset_sizes, None)
     kwargs = {'batch_size': batch_size,
               'num_workers': 1,
               'pin_memory': True,
               'shuffle': True}
+    train_loaders = [torch.utils.data.DataLoader(train_subset, **kwargs) for train_subset in train_subsets]
 
     x_test = datasets.MNIST('data', train=False,
                             transform=transform)
     x_test = torch.utils.data.random_split(x_test, [testset_size, len(x_test) - testset_size], None)[0]
     test_loader = torch.utils.data.DataLoader(x_test, **kwargs)
 
-    avg_model = Net()
-
     avg_model_scores = np.zeros(epochs)
     majority_vote_scores = np.zeros(epochs)
     model_scores = np.zeros((epochs, n_clients))
 
+    avg_model = Net()
+    optimizer_params = None
+
     for epoch in range(epochs):
-        print(f"Epoch {epoch + 1}")
+        print(f"Epoch {epoch}")
         models = [copy.deepcopy(avg_model) for _ in range(n_clients)]
-
-        for i in range(n_clients):
-            print(f"Training model {i + 1}...")
-            train_loader = torch.utils.data.DataLoader(train_subsets[i], **kwargs)
-            models[i] = train_and_return(models[i], train_loader, device, subepochs, plot=plot)
-
+        models, optimizer_params = train_models(models, train_loaders, device, subepochs, optimizer_params=optimizer_params)
         avg_model = average_models(models)
-        print("\nAggregated model:")
+
         avg_model_scores[epoch] = test(avg_model, device, test_loader)
 
         outputs, targets = all_outputs(models, device, test_loader)
@@ -157,6 +164,7 @@ def experiment(batch_size,
         plt.plot(x, y4, c="orange", label="Worst individual model")
         plt.errorbar(x, y5, yerr=stds, c="blue", label="Average of individual models")
         plt.legend()
+        plt.title(f"|x_train|={dataset_size}, |x_test|={testset_size}, batch_size={batch_size}, batches_per_client={batches_per_client}")
         plt.ylim(0, 100)
         plt.xlabel("Epoch")
         plt.ylabel("Score (%)")
@@ -169,5 +177,4 @@ if __name__ == '__main__':
                dataset_size=12800,
                testset_size=1000,
                epochs=20,
-               subepochs=1,
-               plot=False)
+               subepochs=1)
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import copy
+import torch
 
 
 def most_frequent_in_rows(a):
@@ -6,3 +8,22 @@ def most_frequent_in_rows(a):
     for i, row in enumerate(a):
         out[i, 0] = np.bincount(row.astype(int)).argmax()
     return out
+
+
+def average_models(models):
+    model = copy.deepcopy(models[0])
+    sd = model.state_dict()
+    for key in sd:
+        sd[key] = sum([model.state_dict()[key] for model in models]) / len(models)
+    model.load_state_dict(sd)
+    return model
+
+
+def average_optimizers(optimizers):
+    optimizer_params = [optimizer.param_groups[0]["params"] for optimizer in optimizers]
+    mean_params = []
+    for i in range(len(optimizer_params[0])):
+        layers_i = torch.stack([p[i] for p in optimizer_params])
+        mean = torch.mean(layers_i, dim=0)
+        mean_params.append(mean)
+    return mean_params
